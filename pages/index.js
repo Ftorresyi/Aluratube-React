cbd4a0d865e4bb97c55206edc3a1de6e08332b2d
Ftorresyi <yindiyft@gmail.com>
2022-11-08 15:15:09 -0300

    versao final aula 1

--- pages/index.js
+++ pages/index.js
@@ -1,90 +1,109 @@
-import config from "../config.json"
+import config from "../config.json";
 import styled from "styled-components";
+import { CSSReset } from "../src/components/CSSReset";
+import Menu from "../src/components/Menu";
+import { StyledTimeline } from "../src/components/Timeline";
 
-function HomePage(){
+function HomePage() {
+    const estilosDaHomePage = {
+        // backgroundColor: "red" 
+    };
 
-    const estilosDaHomePage = {backgroundColor:"red"};
-    
-    //console.log(config.playlists);
+    // console.log(config.playlists);
 
     return (
-    
-    <div style={estilosDaHomePage}>
-        <Menu/>
-        <Header/>
-        <TimeLine playlists={config.playlists}/>
-
-    </div>
-    
+        <>
+            <CSSReset />
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                flex: 1,
+                // backgroundColor: "red",
+            }}>
+                <Menu />
+                <Header />
+                <Timeline playlists={config.playlists}>
+                    Conteúdo
+                </Timeline>
+            </div>
+        </>
     );
-    
 }
+
 export default HomePage
 
+// function Menu() {
+//     return (
+//         <div>
+//             Menu
+//         </div>
+//     )
+// }
 
-function Menu(){
-    return (
-        <div>
-            Menu
-        </div>
-    );
-}
 
 const StyledHeader = styled.div`
-    	img{
-            width: 80px;
-            height: 80px;
-            border-radius: 50%;
-        }
-        .user-info{
-            display: flex;
-            align-items: center;
-            width: 100%;
-            padding: 16px 32px;
-            gap: 16px;
-        }
+    img {
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+    }
+    .user-info {
+        margin-top: 50px;
+        display: flex;
+        align-items: center;
+        width: 100%;
+        padding: 16px 32px;
+        gap: 16px;
+    }
 `;
-
-function Header(){
+function Header() {
     return (
         <StyledHeader>
-            {/*<img src="banner"/>*/}
-
-            <section className=".user-info">
-            
-                <img src={`http://github.com/${config.github}.png`}/>
+            {/* <img src="banner" /> */}
+            <section className="user-info">
+                <img src={`https://github.com/${config.github}.png`} />
                 <div>
-                    <h2>{config.name}</h2>
-                    <p>{config.job}</p>
+                    <h2>
+                        {config.name}
+                    </h2>
+                    <p>
+                        {config.job}
+                    </p>
                 </div>
-                           
-                
             </section>
-            
         </StyledHeader>
-    );
+    )
 }
 
-function TimeLine(props){
-   // console.log("Dentro do componente", props.playlists);
-    const playlistNames = Object.keys(props.playlists);
-
+function Timeline(propriedades) {
+    // console.log("Dentro do componente", propriedades.playlists);
+    const playlistNames = Object.keys(propriedades.playlists);
+    // Statement
+    // Retorno por expressão
     return (
-        <div>
+        <StyledTimeline>
             {playlistNames.map((playlistName) => {
-                const videos = props.playlists[playlistName];
-                return {videos.map((video) => {
-                        <a href={video.url} >
-                            <img src={video.thumb} />
-                            <span>
-
-                            </span>
-
-                        </a>
-                })}
+                const videos = propriedades.playlists[playlistName];
+                console.log(playlistName);
+                console.log(videos);
+                return (
+                    <section>
+                        <h2>{playlistName}</h2>
+                        <div>
+                            {videos.map((video) => {
+                                return (
+                                    <a href={video.url}>
+                                        <img src={video.thumb} />
+                                        <span>
+                                            {video.title}
+                                        </span>
+                                    </a>
+                                )
+                            })}
+                        </div>
+                    </section>
+                )
             })}
-        </div>
-    );
+        </StyledTimeline>
+    )
 }
-
-
